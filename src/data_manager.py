c5075ff1f96d4b4d63d5d13c2f959021f0c145b6
Infatum <qwertty459@gmail.com>
2017-10-12 23:40:10 +0300

    Flattening and standardization of the dataset

--- src/data_manager.py
+++ src/data_manager.py
@@ -1,8 +1,39 @@
-import numpy as np
-import train_images_data as X
-import train_labels_data as Y
+import load_data
 
 
-def load_data():
-    train_set_images = np.array(X.train_img_data[:])
-    train_set_labels = np.array(Y.train_img_data)
+class Data_Resolver:
+
+    def __init__(self, standardize=True):
+        """
+        Instantiates Data_Resolver instance and loads image labeled dataset for the supervised learning purposes
+
+        """
+        train_img_data, train_l_data, test_img_data, test_l_data, classes = self.flatten_data(self.__load_data())
+        if standardize is True:
+            train_img_data, test_img_data = self.standardize_data(train_img_data, test_img_data)
+        self.train_image_data, self.train_label_data = train_img_data, train_l_data
+        self.test_image_data, self.test_label_data, self.classes = test_img_data, test_l_data, classes
+
+        self.training_examples = train_img_data.shape[0]
+        self.image_size = train_img_data.shape[1] # containing image resolution and color channels
+        self.test_examples = test_img_data[0]
+
+    def __load_data(self):
+        train_img_data, train_label_data, test_img_data, test_label_data, classes = load_data.load_data()
+        return train_img_data, train_label_data, test_img_data, test_label_data, classes
+
+    def flatten_data(self, data):
+        train_img_data, train_label_data, test_img_data, test_label_data, classes = data
+        train_img_data_flatten = train_img_data.reshape(train_img_data[0], -1).T
+        test_img_data_flatten = test_img_data.reshape(test_img_data[0], -1).T
+        return train_img_data_flatten, train_label_data, test_img_data_flatten, test_label_data, classes
+
+    def standardize_data(self, train_img_data, test_img_data):
+        train_img_std = train_img_data / 255
+        test_img_std = test_img_data / 255
+        return train_img_std, test_img_std
+
+
+
+
+
